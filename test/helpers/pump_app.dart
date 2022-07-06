b5c48f8f02f140205bf295a816ea305393492304
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-07-06 11:36:07 +0200

    almost 40% coverage

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) {
+  Future<void> pumpApp(Widget widget, [ThemeData? theme]) {
     return pumpWidget(
-      MaterialApp(home: widget),
+      MaterialApp(home: widget, theme: theme),
     );
   }
 }
